be3c22c2a55430c62b896c391520516ea0423ee1
Mg Thant <tzinwin517@gmail.com>
2025-10-17 16:11:38 +0630

    custom-redux-store-dispatch-actions

--- custom-redux-store/src/App.jsx
+++ custom-redux-store/src/App.jsx
@@ -1,16 +1,18 @@
-import createStore from "./store/customStore"
+import store from "./store/customStore";
+import * as actions from "./actions/actions";
 
 function App() {
-  let store = createStore();
-  console.log(store)
+  console.log(store);
   store.state = 1;
-  console.log(store.getState())
+  console.log(store.getState());
+  store.dispatch(actions.bugAdded("Bug 1"));
+  console.log(store.getState());
 
   return (
     <>
       <h1>Custom Store</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- custom-redux-store/src/store/customStore.js
+++ custom-redux-store/src/store/customStore.js
@@ -1,13 +1,22 @@
-const createStore = () => {
-  let state;
+import reducer from "../reducers/reducer";
+
+const createStore = (reducer) => {
+  let state = [];
 
   const getState = () => {
     return state;
   };
 
+  const dispatch = (action) => {
+    // Call reducer function to get new state
+    // Notify state changes to subscribers
+    state = reducer(state, action);
+  }
+
   return {
-    getState
+    getState,
+    dispatch
   };
 };
 
-export default createStore;
+export default createStore(reducer);
