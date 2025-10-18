b312b3a100a0c1f20a6c0b58466722fa16b92e0c
Mg Thant <tzinwin517@gmail.com>
2025-10-18 22:27:05 +0630

    complete-duck-pattern

--- redux-starter/src/App.jsx
+++ redux-starter/src/App.jsx
@@ -1,18 +1,22 @@
-import store from "./store/store";
-import * as actions from "./store/actions";
+import configureStore from "./store/configureStore";
+import { bugAdded, bugRemoved, bugResolved} from "./store/bugs";
 
 function App() {
+  const store = configureStore();
+
   const unsubscribe = store.subscribe(() => {
     console.log("Store has changed.", store.getState());
   }); // return a function that remove listening process
 
-  store.dispatch(actions.bugAdded("Bug 1"));
+  store.dispatch(bugAdded("Bug 1"));
 
   // unsubscribe(); // remove listenging or remove subscribe
 
-  // store.dispatch(bugRemoved(1));
+  store.dispatch(bugAdded("Bug 2"));
 
-  store.dispatch(actions.bugResolved(1));
+  store.dispatch(bugRemoved(2));
+
+  store.dispatch(bugResolved(1));
 
   return <></>;
 }
