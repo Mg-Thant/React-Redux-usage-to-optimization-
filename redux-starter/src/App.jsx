e7b152ece06f6b890ad94a4100d88b92620b234b
Mg Thant <tzinwin517@gmail.com>
2025-10-18 21:08:48 +0630

    import-fix

--- redux-starter/src/App.jsx
+++ redux-starter/src/App.jsx
@@ -1,18 +1,18 @@
 import store from "./store/store";
-import { bugAdded, bugRemoved, bugResolved } from "./actions/actions";
+import * as actions from "./store/actions";
 
 function App() {
   const unsubscribe = store.subscribe(() => {
     console.log("Store has changed.", store.getState());
   }); // return a function that remove listening process
 
-  store.dispatch(bugAdded("Bug 1"));
+  store.dispatch(actions.bugAdded("Bug 1"));
 
   // unsubscribe(); // remove listenging or remove subscribe
 
   // store.dispatch(bugRemoved(1));
 
-  store.dispatch(bugResolved(1));
+  store.dispatch(actions.bugResolved(1));
 
   return <></>;
 }
