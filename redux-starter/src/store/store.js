5f4751eb8a3b8b0f335fbe746d17bf9c4133ad4b
Mg Thant <tzinwin517@gmail.com>
2025-10-18 21:04:07 +0630

    file-structure-change-into-duck-pattern

--- redux-starter/src/store/store.js
+++ redux-starter/src/store/store.js
@@ -1,6 +1,7 @@
 import { legacy_createStore as createStore } from "redux";
-import reducer from "../reducers/reducer";
+import reducer from "./reducer";
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
